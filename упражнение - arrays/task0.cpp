36b14b1ab0b53ccf89f89b3630e39acbc818aaab
Filip Stoyanov <filip.stoyanov@pytek.bg>
2022-01-06 01:14:53 +0200

    Dictionary task features

--- упражнение - arrays/task0.cpp
+++ упражнение - arrays/task0.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <cmath>
 using namespace std;
 char **allocate_char_matrix(unsigned height, unsigned width)
 {
@@ -10,36 +11,80 @@ char **allocate_char_matrix(unsigned height, unsigned width)
     }
     return table;
 }
-char *begin_word(char *word, char *sentence)
+bool is_letter(char c)
+{
+    if ((c >= 'a' && c <= 'z') ||
+        (c >= 'A' && c <= 'Z'))
+    {
+        return true;
+    }
+    return false;
+}
+bool is_punctuation_mark(char c)
+{
+    if (c == '.' || c == '?' || c == '!')
+    {
+        return true;
+    }
+    return false;
+}
+bool is_arithmetic_operation(char c)
+{
+    if (c != '-' && c != '+' && c != '*' && c != '>' && c != '<' && c != '/')
+    {
+        return false;
+    }
+    return true;
+}
+int begin_word(char *word, char *sentence)
 {
     unsigned length_word = strlen(word);
     unsigned length_sentence = strlen(sentence);
     unsigned reps{0};
-    while (*sentence != '\0')
+    int counter{0};
+    while (counter < length_sentence)
     {
         reps = 0;
-        char *begin_pointer = sentence;
-        for (unsigned i{0}; i < length_word && word[i] == *sentence; ++i)
+        int begin_counter = counter;
+        char *prev_symbol = sentence;
+        --prev_symbol;
+        if (!is_letter(*prev_symbol))
         {
-            ++reps;
-            ++sentence;
+            for (unsigned i{0}; i < length_word && word[i] == *(sentence + counter); ++i)
+            {
+                ++reps;
+                ++counter;
+            }
         }
-        if (reps == length_word)
+        if (reps == length_word && !is_letter(*(sentence + counter)))
         {
-            return begin_pointer;
+            return begin_counter;
         }
-        ++sentence;
+        ++counter;
     }
-    return nullptr;
+    return -1;
 }
-void replace_word(char *new_word, char *old_word, char *sentence)
+void replace_words(char *old_word, char *new_word, int pos, char *&sentence)
 {
-    char *beg = begin_word(old_word, sentence);
-    if (beg != nullptr)
+    int sentence_length = strlen(sentence);
+    int length_old_word = strlen(old_word);
+    int length_new_word = strlen(new_word);
+    int number_symbols = length_new_word - length_old_word;
+    int diff_index;
+    char *copy_sentence = new char[1024];
+    strcpy(copy_sentence, sentence);
+    sentence_length += number_symbols;
+    for (int i{pos + length_new_word}; i < sentence_length; ++i)
     {
-        //check new word is longer than the old one
-        //replace word
+        sentence[i] = copy_sentence[i - number_symbols];
     }
+    int j{pos};
+    sentence[sentence_length] = '\0';
+    for (int i{0}; i < length_new_word; ++i)
+    {
+        sentence[j + i] = new_word[i];
+    }
+    delete[] copy_sentence;
 }
 void autoCorrect(unsigned n, unsigned dictionary_length)
 {
@@ -87,70 +132,65 @@ void autoCorrect(unsigned n, unsigned dictionary_length)
             new_word[k - j - 1] = line[k];
         }
         new_word[length_line - j - 1] = '\0';
-        char *beg = strstr(text, old_word);
-        unsigned size_old = strlen(old_word);
-        unsigned size_new = strlen(new_word);
-        if (beg != nullptr)
+        int pos{-1};
+        do
         {
-            strncpy(beg, new_word, size_new);
-        }
+            pos = begin_word(old_word, text);
+            if (pos > -1)
+            {
+                replace_words(old_word, new_word, pos, text);
+            }
+        } while (pos != -1);
     }
     char *edit_text = new char[2048];
     unsigned count_quotation_mark{0};
     unsigned counter{0};
-    for (unsigned i{0}; i < n; ++i)
+    unsigned text_length = strlen(text);
+    for (unsigned i{0}; i < text_length; ++i)
     {
-        if (text[i] == '.' || text[i] == '?' || text[i] == '!' || text[i] == ',' || text[i] == '-')
+        if ((is_punctuation_mark(text[i]) || text[i] == ',' || is_arithmetic_operation(text[i])) && text[i + 1] != ' ')
         {
             edit_text[counter] = text[i];
             ++counter;
             edit_text[counter] = ' ';
+            ++counter;
         }
         else if (text[i] == '"')
         {
             ++count_quotation_mark;
-            if (count_quotation_mark % 2 == 1)
-            {
-                edit_text[counter] = ' ';
-                ++counter;
-                edit_text[counter] = text[i];
-            }
+            edit_text[counter] = '"';
+            ++counter;
         }
-        else if (text[i] == '"')
-        {
-            ++count_quotation_mark;
-            if (count_quotation_mark % 2 == 1)
-            {
-                edit_text[counter] = ' ';
-                ++counter;
-                edit_text[counter] = text[i];
-            }
-        }
-        else if (text[i] == '(' || text[i] == '>' || text[i] == '*' || text[i] == '-' || text[i] == '+' || text[i] == '/')
+       else if (text[i] != ' ' && !is_punctuation_mark(text[i]) && text[i] != ',' && !is_arithmetic_operation(text[i])  && count_quotation_mark % 2 == 0 && text[i + 1] == '"')
         {
             edit_text[counter] = ' ';
             ++counter;
+            edit_text[counter] = '"';
+            ++counter;
+        }
+        else if (text[i] != ' ' && (text[i + 1] == '(' || is_arithmetic_operation(text[i + 1])))
+        {
+            edit_text[counter] = ' ';
+            ++counter;
+            edit_text[counter] = text[i + 1];
+            ++counter;
+        }
+        else if ( (!is_punctuation_mark(text[i]) && text[i+1] == '"' && count_quotation_mark % 2 == 1)
+                  || (!is_punctuation_mark(text[i]) && (text[i+1] == '\n' || text[i+1] == '\0')))
+        {
             edit_text[counter] = text[i];
-        }
-        else if (text[n - 1] != '.' || text[n - 1] != '?' || text[n - 1] != '!' || text[n - 1] != '"')
-        {
-            edit_text[counter] = '.';
-        }
-        else if (text[i] == '\n')
-        {
-            char c = edit_text[counter];
+            ++counter;
             edit_text[counter] = '.';
             ++counter;
-            edit_text[counter] = text[i];
         }
         else
         {
             edit_text[counter] = text[i];
+            ++counter;
         }
-        ++counter;
     }
     edit_text[counter] = '\0';
-    std::cout << edit_text << std::endl;
+    std::cout<<edit_text<<std::endl;
     delete[] edit_text;
     delete[] line;
     delete[] old_word;
@@ -161,9 +201,18 @@ int main()
 {
     unsigned text_length, dictionary_length;
     autoCorrect(text_length, dictionary_length);
-    //    char* str = new char[100];
-    //    std::cin>>str;
-    //    char* sentence = new char[100];
-    //    std::cin>>sentence;
+    // char *str1 = new char[100];
+    // std::cin >> str1;
+    // char *str2 = new char[100];
+    // std::cin >> str2;
+    // char *sentence = new char[100];
+    // std::cin.clear();
+    // std::cin.ignore();
+    // std::cin.getline(sentence, 100);
+    // replace_words(str1,str2,sentence);
+    // std::cout<<sentence;
+    // delete[] str1;
+    // delete[] str2;
+    // delete[] sentence;
     return 0;
 }
